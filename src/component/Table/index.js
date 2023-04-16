548a20368846244a00f6bc0ce79a9dbbab129448
feralonsomaccari <feralonsomaccari@gmail.com>
2023-04-16 14:43:27 +0300

    Add Table component

---
+++ src/component/Table/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
+const Table = ({ headers, children }) => {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {headers.map((header, index) => <th key={index}>{header}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {children}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
