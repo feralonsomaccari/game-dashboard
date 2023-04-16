0e5809e221cd73e1c33cd80b021cd1131234441b
feralonsomaccari <feralonsomaccari@gmail.com>
2023-04-16 18:05:08 +0300

    Update styles

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,16 +1,33 @@
 import styles from "./App.module.css";
 import Table from "../Table"
-import dummyTableData from "../Table/usersDummyData.json"
+import Counter from "../Counter"
+import usersDummyData from "../Table/usersDummyData.json"
+import gamesDummyData from "../Table/gamesDummyData.json"
 
 function App() {
   return (
     <div className={styles.wrapper}>
+      <div className={styles.countersWrapper}>
+        <Counter value={usersDummyData.length} title="Registered users" />
+        <Counter value={gamesDummyData.length} title="Available games" />
+      </div>
       <Table headers={['Username', 'Email', 'Creation Date']}>
-        {dummyTableData.map((row) => {
+        {usersDummyData.map((row) => {
           return (
             <tr key={row.id}>
               <td data-th='Username'>{row.username}</td>
               <td data-th='Email'>{row.email}</td>
+              <td data-th='Creation Date'>{row.created_at}</td>
+            </tr>)
+        })}
+      </Table>
+      <Table headers={['Name', 'Category', 'Developer', 'Creation Date']}>
+        {gamesDummyData.map((row) => {
+          return (
+            <tr key={row.id}> 
+              <td data-th='Name'>{row.name}</td>
+              <td data-th='Category'>{row.category}</td>
+              <td data-th='Developer'>{row.developer}</td>
               <td data-th='Creation Date'>{row.created_at}</td>
             </tr>)
         })}
