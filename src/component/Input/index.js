055ce42d3b9647e8c5ec21039817b258d8266edd
feralonsomaccari <feralonsomaccari@gmail.com>
2023-04-16 20:43:03 +0300

    Update a11y for Input component

--- src/component/Input/index.js
+++ src/component/Input/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import styles from './Input.module.css'
 
 const Input = ({ placeholder, onChangeHandler }) => (
-    <input className={styles.input} placeholder={placeholder} onChange={onChangeHandler} data-testid="input" />
+    <input className={styles.input} placeholder={placeholder} onChange={onChangeHandler} data-testid="input" aria-label="filter table data" />
 )
 export default Input
