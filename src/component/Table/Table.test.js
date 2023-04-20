30a5e2f2344345798f80cacdf3a97223508a111d
feralonsomaccari <feralonsomaccari@gmail.com>
2023-04-20 21:16:38 +0300

    Update Table unit Tests

--- src/component/Table/Table.test.js
+++ src/component/Table/Table.test.js
@@ -1,26 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import dummyTableData from "../Table/usersDummyData.json"
+import dummyTableData from "../../services/dummyData/usersDummyData.json"
 
 import Table from "./";
 
+const headers = {
+    "name": "Name",
+    "category": "Category",
+    "developer": "Developer",
+    "created_at": "Created At"
+}
+
 describe("<Table/>", () => {
     it("should render a Table component", () => {
-        render(<Table headers={['Username', 'Email', 'Creation Date']} />);
+        render(<Table headers={headers} />);
 
         const table = screen.queryByRole("table");
         expect(table).toBeInTheDocument();
     });
 
-    it("should not render a Table component", () => {
-        render(<Table />);
-
-        const table = screen.queryByRole("table");
-        expect(table).not.toBeInTheDocument();
-    });
-
     it("should render a Table component right amount of items", () => {
-        render(<Table headers={['Username', 'Email', 'Creation Date']}>
-            {dummyTableData.map((row) => {
+        render(<Table headers={headers}>
+            {dummyTableData.data.map((row) => {
                 return (
                     <tr key={row.id}>
                         <td data-th='Username'>{row.username}</td>
@@ -30,12 +30,12 @@ describe("<Table/>", () => {
             })}
         </Table>);
         const table = screen.queryAllByRole("row");
-        expect(table).toHaveLength(dummyTableData.length + 1); // Plus one from the head
+        expect(table).toHaveLength(dummyTableData.data.length + 1); // Plus one from the head
     });
 
     it("should render a Table component with an extra prop of aria-label", () => {
         const ariaLabel = "List of registered users"
-        render(<Table headers={['Username', 'Email', 'Creation Date']} extraProps={{ 'aria-label': ariaLabel }} />);
+        render(<Table headers={headers} extraProps={{ 'aria-label': ariaLabel }} />);
 
         const table = screen.getByRole("table").getAttribute('aria-label');
         expect(table).toEqual(ariaLabel);
